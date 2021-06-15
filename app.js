3a32dd5675e5d9e8cbd9852c31c00590b357707e
SCVORAl <systemked@mail.ru>
2021-06-15 10:01:07 +0300

    last

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-// app.use(express.json({ extended: true }))
-
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routers'))
 app.use('/api/users', require('./routes/users.routers'))
 
-// const PORT = config.get('port') || 5000
-const PORT = process.env.PORT || 5000
+const PORT = config.get('port') || 5000
 
 async function start() {
   try {
